0588f62904add81e434853787634076e1c92a371
Paul Issott <paul@slitaz.org>
2013-07-18 19:35:25 +0100

    Tiny edits

--- etc/slitaz/firewall.sh
+++ etc/slitaz/firewall.sh
@@ -19,33 +19,33 @@ iptables -A INPUT -i lo -j ACCEPT
 # Accept input on the local network
 iptables -A INPUT -s $LOCAL_NETWORK -j ACCEPT
 
-# Accept near all output trafic.
+# Accept (nearly) all output trafic
 iptables -A OUTPUT -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
 
-# Accept input trafic only for connections initialized by user.
+# Accept input trafic only for connections initialized by user
 iptables -A INPUT -m conntrack  --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 # If you manage a HTTP/SSH/FTP/IRC server you can accept input for
-# non-established connections an some ports. Else you can disable the
-# lines below for more secured setup.
+# non-established connections on some ports. Else you can disable the
+# lines below for a more secure setup
 for iface in $INTERFACES
 do
-	# Accept input on port 80 for the HTTP server.
+	# Accept input on port 80 for the HTTP server
 	iptables -A INPUT -i $iface -p tcp --source-port 80 -j ACCEPT
 
-	# Accept input on port 22 for SSH.
+	# Accept input on port 22 for SSH
 	iptables -A INPUT -i $iface -p tcp --destination-port 22 -j ACCEPT
 
-	# Accept port 21 and, 1024 to 60310 for FTP.
+	# Accept port 21 and 1024 to 60310 for FTP
 	iptables -A INPUT -i $iface -p tcp --destination-port 21 -j ACCEPT
 	iptables -A INPUT -i $iface -p tcp --destination-port 1024:60310 -j ACCEPT
 
-	# Accept port 6667 for IRC chat.
+	# Accept port 6667 for IRC chat
 	iptables -A INPUT -i $iface -p tcp --source-port 6667 -j ACCEPT
 
-	# Accept unprivileged ports.
+	# Accept unprivileged ports
 	iptables -A INPUT -i $iface -p udp --destination-port 1024:65535 -j ACCEPT
 
-	# Accept ping.
+	# Accept ping
 	iptables -A INPUT -i $iface -p icmp -j ACCEPT
 done
